7f5ec451c268a226abb8d0a436228ba79bcfc82f
sky00high <tianyu0726@gmail.com>
2017-02-26 13:28:05 -0500

    constacts post works

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,8 +1,8 @@
 var express = require('express');
+
 var router = express.Router();
 var Contact = require('../../models/contact.js');
 var Email = require('../../models/email.js');
-
 
 
 router.post('/', function(req,res,next){
@@ -23,71 +23,62 @@ router.post('/', function(req,res,next){
 	}
 	var emailPromise = new Promise(function(resolve, reject){
 		Email.getItems(emails, function(err, emails){
-
-			if(err){
-				reject(err);
-			}else{
-				resolve(emails);
-			}
-			
+			if(err)reject(err);
+			else resolve(emails);
 		});
-	});
-
-	emailPromise.then(function(emailsInDatabase){
+	}).then( function(emails){
 
 
-		var promise = new Promise(function(resolve, reject){
-			if(emailsInDatabase.length != 0){
-				reject({error: "some email already existed in database", existed_emails: emailsInDatabase});
-			} else{
-				Contact.create(req.body.contacts, function (err, contacts){
-					if(err){
-						reject({
-							error: "contacts creation",
-							detail: err.details,
-							created: err._object
-						});
-					} else{
-						var emails = [];
-						for(var i = 0; i < contacts.length; i++){
-							for(var j = 0; j < contacts[i].attrs.emails.length; j++){
-								emails.push({
-									email: contacts[i].attrs.emails[j],
-									owner: req.user.username,
-								});
-							}
-						}
-						resolve({emails: emails, err: err});
-					}
-
+		if(emails.length != 0){
+			res.status(400).send({reason: "Concats with following emails are already created. Please delete them before import.", emails: emails});
+			return;
+		}
+		// Because the promises all and dynogels's fail fast batch async behavior, I need
+		// to construct my manual promise all list in order to get the list of items that
+		// was succesfully created. 
+		var promises = [];
+		for(var i  = 0; i < req.body.contacts.length; i++){
+			promises.push(new Promise(function(resolve, reject){
+				Contact.create(req.body.contacts[i], function(err, contact){
+					if(err) resolve({status: "errored",reason: err,contact: err._object});
+					else resolve({ status: "passed", contact: contact.get()});
 				});
-			}
-		});
+			}));
+		}
 
-		return promise;
-
-	}).then(function(resolveData){
-		print("lalala");
-		Email.create(resolveData.emails,function(err, emails){
-			if(err){
-				//Since we did all the check, this really shouldn't happen
-				res.status(500).send("email creating error, this shouldn't happen " + err);
-			} else{
-				if(resolveData.err){
-					res.status(400).send("err: " + resolveData.err);
-				} else{
-					res.status(200).send("contacts created");
+		Promise.all(promises).then(values =>{
+			
+			var emails = [];
+			var errMessage = [];
+			for(var i = 0; i < values.length; i++){
+				if(values[i].status == 'passed'){
+					emails = emails.concat(values[i].contact.emails.map(elem => { return {owner: req.user.username, email:elem}}));
+				}else{
+					errMessage.push({
+						contact: values[i].contact,
+						reason: values[i].reason.details
+					});
 				}
 			}
-
+			Email.create(emails,function(err, emails){
+				if(err){
+					//Since we did all the check, and the contacts with those emails passed validation in the previous phase,
+					// this really shouldn't happen 
+					res.status(500).send("email creating error, " + err);
+				} else{
+					if(errMessage.length != 0){
+						res.status(400).send(errMessage);
+					} else{
+						res.status(200).send("contacts created");
+					}
+				}
+			});
 
 
 		});
-	}).catch(function(err){
+	}).catch(err =>{
 		res.status(400).send(err);
 	});
-
-	
 
 
 
