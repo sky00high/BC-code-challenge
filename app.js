cb15509c941ce66c5dfe28e0e5b0c153cdd5d443
sky00high <tianyu0726@gmail.com>
2017-02-26 22:48:20 -0500

    update path

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(expressJWT({secret:'secret'}).unless({path:['/login', '/users']}));
+app.use(expressJWT({secret:'secret'}).unless({path:['/', '/login', '/users']}));
 
 app.use('/users', users);
 app.use('/api/contacts', contacts);
